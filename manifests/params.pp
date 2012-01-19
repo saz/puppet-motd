666a7658e48fa05b09eec64283f363b514bd8a83
Steffen Zieger <me@saz.sh>
2012-01-19 17:06:26 +0100

    this module is motd, not locales ;)

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class motd::params {
   case $::operatingsystem {
     ubuntu, debian: {
       $config_file = '/etc/motd'
-      $template = 'locales/motd.erb'
+      $template = 'motd/motd.erb'
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
