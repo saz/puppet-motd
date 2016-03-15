9cc9a07a33e65df723a5a3da5aeccfae232aaf30
PUPPBJS <it-aix@bjs.com>
2016-03-15 14:01:31 -0400

    Update params.pp
    added owner and group for both AIX and Linux

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,20 +3,32 @@ class motd::params {
     'AIX': {
       $config_file = '/etc/motd'
       $template = 'motd/motd.erb'
+      $mode = '0644'
+      $owner = 'root'
+      $group = 'bin'
     }
       redhat, debian, suse, gentoo : {
       $config_file = '/etc/motd'
       $template = 'motd/motd.erb'
+      $mode = '0644'
+      $owner = 'root'
+      $group = 'root'
     }
     default: {
       case $::operatingsystem {
         'AIX': {
           $config_file = '/etc/motd'
           $template = 'motd/motd.erb'
+          $mode = '0644'
+          $owner = 'root'
+          $group = 'bin'
         }
         gentoo: {
           $config_file = '/etc/motd'
           $template = 'motd/motd.erb'
+          $mode = '0644'
+          $owner = 'root'
+          $group = 'root'
         }
         default: {
           fail("The ${module_name} module is not supported on ${::osfamily}/${::operatingsystem}.")
